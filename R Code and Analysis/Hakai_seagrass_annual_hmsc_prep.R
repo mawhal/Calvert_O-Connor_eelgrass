903927cad04ebcc78f9767077707460cbc9eb493
Matt Whalen <mawhal@gmail.com>
2019-11-23 09:42:09 -0800

    2017 grazer prep for HMSC

--- R Code and Analysis/Hakai_seagrass_annual_hmsc_prep.R
+++ R Code and Analysis/Hakai_seagrass_annual_hmsc_prep.R
@@ -37,12 +37,15 @@ mfilt <- m %>%
 
 
 ## 2017 data to start
+m17 <- mfilt %>% 
+  filter( year==2017 )
 
-
+# which data to use
+muse <- m17
 
 
 # summarize taxon counts per sample
-m.sum <- mfilt %>% 
+m.sum <- muse %>% 
   # unite( "ID", year,site,sample, remove=FALSE ) %>% 
   group_by( year, site, sample, taxon4 ) %>% 
   summarize( abundance=length(size) )
@@ -58,53 +61,8 @@ meta <- data.frame(m.meta[,c(1:3)])
 comm <- data.frame(m.meta[,-c(1:3)])
 names(comm) <- make.cepnames( names(comm) )
 
-# richness and diversity
-meta$richness <- specnumber(comm)
-meta$shannon <- diversity(comm)
-
-# add geolocation
-geo <- read_csv( "../metadata/coreen_hakai_sites_location_2015.csv" )
-geo <- geo %>% 
-  select( site, lat, long )
-
-meta <- left_join( meta, geo )
-meta$site <- fct_reorder( meta$site, -meta$lat )
 
 
-# nmds of comparable data
-
-mds <- metaMDS( comm, distance="bray", k=7 )
-mds # very high stress with 2 axes 
-
-# extract points for first two axes
-meta <- cbind(meta, mds$points[,c(1,2)])
-
-# convex hull
-StatChull <- ggproto("StatChull", Stat,
-                     compute_group = function(data, scales) {
-                       data[chull(data$x, data$y), , drop = FALSE]
-                     },
-                     
-                     required_aes = c("x", "y")
-)
-
-stat_chull <- function(mapping = NULL, data = NULL, geom = "polygon",
-                       position = "identity", na.rm = FALSE, show.legend = NA, 
-                       inherit.aes = TRUE, ...) {
-  layer(
-    stat = StatChull, data = data, mapping = mapping, geom = geom, 
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
-  )
-}
-
-meta$site2 <- factor( meta$site, ordered=T )
-man.col <- viridis::viridis(5)
-cols <- c( 'black','black',man.col[2],man.col[2],man.col[2],
-           man.col[3],man.col[3],man.col[4],man.col[4],man.col[5] )
-ggplot( meta, aes(MDS1,MDS2,col=site)) + 
-  # stat_chull( fill=NA ) +
-  stat_ellipse( aes(lty=site), lwd=1 ) +
-  geom_point( size=3 ) +
-  scale_linetype_manual( values=c(1,2,1,2,3,1,2,1,2,1)) +
-  scale_color_manual( values=cols  )
+# write metadata and community data to disk
+write_csv( comm, "output data/Hakai_2017_mesograzer_comm.csv" )
+write_csv( meta, "output data/Hakai_2017_mesograzer_meta.csv" )
