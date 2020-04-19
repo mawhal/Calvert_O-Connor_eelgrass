b2758ae83544748fe18a347ddb545f0570d80e77
Matt Whalen <mawhal@gmail.com>
2020-04-19 14:36:46 -0700

    eelgrass PCA in eelgrass_summary.R; move 2015 quadrat names to metadata

---
+++ R_Code_and_Analysis/eelgrass_summary.R
@@ -0,0 +1,107 @@
+###################################################################
+###  Hakai + O'Connor Seagrass Data From Calvert Island 
+###  Dataset begins in 2014 
+###  Samples from several sites (not always the same)
+###
+###  This code produces summaries of eelgrass variables
+###    inlcuding those measured at shoot and quadrat levels
+###
+###  Question: do we have information about the shoots that were
+###            swabbed for microbes?
+###
+### 
+###  started by Matt Whalen  on 19 April 2020
+###  
+###################################################################
+
+
+## libraries
+library(tidyverse)
+
+
+# what to do with 2015? Different number of shoots, not included in quadrat level dataset
+#   can maybe use single shoot data here??
+
+## read data
+# quadrats
+qall <- read_csv( "output_data/emily_hakai_quad_combined_20200205_bia_reviewed.csv" )
+qsel <- qall %>% select( year, site, sample=quadrat_id, shoot.density=quadrat_shoot_density, lai=quadrat_lai, biomass=quadrat_biomass_g )
+# shoots
+sall <- read_csv( "../Data/R Code for Data Prep/master data/O'Connor_hakai_seagrass_MASTER_shoots.csv" )
+ssel <- sall %>% select( year, site, sample=id, shoot.length, shoot.width )
+# bed area and site depth
+abiotic <- read_csv( "../metadata/00_Hakai_UBC_metadata_MASTER - geolocation_site.csv" )
+abiotic$site_name <- tolower(gsub( " ","_", abiotic$site_name ))
+abiotic$site_name[ abiotic$site_name =="inner_choked"] <- "choked_inner"
+abiotic$site_name[ abiotic$site_name =="sandspit"] <- "choked_sandspit"
+abiotic <- abiotic %>% 
+  filter( project=="O'Connor" ) %>% 
+  select( site=site_name, lat, long, depth_m = `depth (m, chart datum)`, area_m2=area_m2_2016 )
+
+# merge
+qs <- full_join( qsel, ssel )
+qs[ apply( qs,1, function(z) any(is.na(z)) ), ] 
+ # complete cases
+qs <- qs[ complete.cases( qs ), ]
+qsa <- left_join( qs, abiotic )
+
+
+# 
+pp <- select(qsa, depth_m, area_m2, shoot.density, biomass, shoot.length, shoot.width)
+ppl <- pp %>% mutate( area_m2=log(area_m2), shoot.density=log(shoot.density), biomass=log(biomass) )
+# pp <- select(qsa, shoot.density, biomass, shoot.length, shoot.width)
+# ppl <- pp %>% mutate(  shoot.density=log(shoot.density), biomass=log(biomass) )
+psych::pairs.panels( ppl, scale = T )
+
+
+# PCA
+pca <- prcomp( ppl )
+summary(pca)
+biplot(pca,choices = c(1,2), pc.biplot = F, scale=0.5 )
+biplot(pca,choices = c(2,3))
+ppp <- data.frame( qsa, pca$x )
+ggplot( ppp, aes(x=PC1,y=PC2,col=site)) + geom_point()
+library( ggfortify )
+autoplot( prcomp(ppl), data=ppp, col='site', 
+          loadings = T, loadings.colour="blue",
+          loadings.label = T, loadings.label.colour="black", 
+          loadings.label.size = 2,  
+          scale = 1  )
+ggsave( "figs/eelgrass_PCA12.png", width=5, height=3 )
+autoplot( prcomp(ppl), data=ppp, col='site', x=2, y=3,
+          loadings = T, loadings.colour="blue",
+          loadings.label = T, loadings.label.colour="black", 
+          loadings.label.size = 2,  
+          scale = 1  )
+ggsave( "figs/eelgrass_PCA23.png", width=5, height=3 )
+autoplot( prcomp(ppl), data=ppp, col='site', x=1, y=4,
+          loadings = T, loadings.colour="blue",
+          loadings.label = T, loadings.label.colour="black", 
+          loadings.label.size = 2,  
+          scale = 1  )
+ggsave( "figs/eelgrass_PCA14.png", width=5, height=3 )
+autoplot( prcomp(ppl), data=ppp, col='site', x=4, y=5,
+          loadings = T, loadings.colour="blue",
+          loadings.label = T, loadings.label.colour="black", 
+          loadings.label.size = 2,  
+          scale = 1  )
+
+# repeat but average across samples within a site
+ppm <- qsa %>% 
+  group_by(year,site) %>% 
+  summarize_all( mean ) %>% 
+  ungroup()
+pp <- select(ppm, depth_m, area_m2, shoot.density, biomass, shoot.length, shoot.width)
+ppl <- pp %>% mutate( area_m2=log(area_m2), shoot.density=log(shoot.density), biomass=log(biomass) )
+psych::pairs.panels( ppl, scale = T )
+
+
+# PCA
+pca <- prcomp( ppl )
+summary(pca)
+autoplot( prcomp(ppl), data=ppm, col='site', 
+          loadings = T, loadings.colour="blue",
+          loadings.label = T, loadings.label.colour="black", 
+          loadings.label.size = 2,  
+          scale = 1  )
+ggsave( "figs/eelgrass_PCAmean12.png", width=5, height=3 )
---
+++ R_Code_and_Analysis/procrustes_test.R
@@ -0,0 +1,63 @@
+### PROCRUSTES microbes and grazers ###
+### Author: Bianca Trevizan Segovia ###
+### Date created: March 02, 2020 ###
+
+library(dplyr)
+library(vegan)
+#library(usedist)
+
+###################################
+############### 2016 ##############
+###################################
+
+#load grazers metadata
+metadata_grazers_2016 <- read.csv("~/PostDoc/projects/Hakai_Quadra_data_retreat/mantel_microbes_grazers/2016_grazer_metadata.csv")
+
+#The row and column names in the distance matrix can be created from metadata$sample using vegan::make.cepnames()
+metadata_grazers_2016$labels <- vegan::make.cepnames(metadata_grazers_2016$sample)
+
+#load grazers bray curtis dissimilarity matrix
+df_grazers_2016 <- read.csv("~/PostDoc/projects/Hakai_Quadra_data_retreat/mantel_microbes_grazers/2016_grazer_braycurtis.csv")
+
+df_grazers_2016$sample <- colnames(df_grazers_2016)
+
+#load 16S microbial distance matrix GENUS
+df_16S_2016_genus <- read.csv("~/PostDoc/projects/Hakai_Quadra_data_retreat/mantel_microbes_grazers/genus_16S_2016_braycurtis.csv")
+df_16S_2016_genus <- df_16S_2016_genus %>% 
+  dplyr::rename("sample" = "X")
+
+#load 18S microbial distance matrix GENUS
+df_18S_2016_genus <- read.csv("~/PostDoc/projects/Hakai_Quadra_data_retreat/mantel_microbes_grazers/genus_18S_2016_braycurtis.csv")
+df_18S_2016_genus <- df_18S_2016_genus %>% 
+  dplyr::rename("sample" = "X")
+
+
+##############################################
+######### PROCRUSTES GRAZERS VS 16S GENUS ######### 
+##############################################
+
+# which samples are common to both matrices?
+labels_grazers_2016 <- as.vector(df_grazers_2016$sample)
+labels_16S_genus_2016 <- as.vector(df_16S_2016_genus$sample)
+
+# filter only samples that are common to both 
+common_samples <- intersect(labels_grazers_2016, labels_16S_genus_2016)
+
+df_grazers_2016_mantel <- df_grazers_2016 %>% 
+  dplyr::filter(sample %in% common_samples)
+dist_grazers_2016 <- dist(df_grazers_2016_mantel, diag = TRUE, upper = FALSE)
+
+df_16S_genus_2016_mantel <- df_16S_2016_genus %>% 
+  dplyr::filter(sample %in% common_samples)
+dist_16S_genus_2016 <- dist(df_16S_genus_2016_mantel , diag = TRUE, upper = FALSE)
+
+
+mds.invert <- monoMDS(dist_grazers_2016)
+mds.16S <- monoMDS(dist_16S_genus_2016)
+proc_invert_16S <- procrustes(mds.invert, mds.16S)
+proc_invert_16S 
+summary(proc_invert_16S)
+plot(proc_invert_16S)
+plot(proc_invert_16S, kind=2)
+residuals(proc_invert_16S)
+protest(mds.invert, mds.16S)
