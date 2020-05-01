9c869ff6d50938891660b378233471ee60feb123
biatsegovia <biatsegovia@gmail.com>
2020-04-30 19:59:53 -0700

    added adipart code for genus and family level 16S

--- R_Code_and_Analysis/Beta_diversity_LCBD_gammapart/gamma_partitioning_microbes.R
+++ R_Code_and_Analysis/Beta_diversity_LCBD_gammapart/gamma_partitioning_microbes.R
@@ -9,6 +9,8 @@ library(dplyr)
 ############ 16S ############
 #############################
 
+## ASV level ###
+
 ### Read table metadata and abundances
 microbes_16S <- read.csv("Data/R_Code_for_Data_Prep/master_data/MASTER_prokary_ASV_level.csv", header=T)
 names(microbes_16S)[1:17]
@@ -19,10 +21,10 @@ lapply(comm.years, dim)
 
 # remove taxa that did not appear in a given year
 comm.zero <- lapply( comm.years, function(z) {
-  tmp <- z[,-c(1:17)]
+  tmp <- z[,-c(1:16)]
   w <- colSums(tmp)>0
   df <- tmp[, w ]
-  return( data.frame(z[,c(1:17)],df))
+  return( data.frame(z[,c(1:16)],df))
 })
 
 meta <- lapply(comm.zero, function(z) z[,1:16])
@@ -62,6 +64,86 @@ g1 + geom_bar(data=add.plot2 %>% dplyr::filter(key == "null"),
               aes(x=year + 0.15, fill=level),
               stat="identity", position="stack", width=0.3, alpha=0.5)
 ggsave( "R_Code_and_Analysis/figs/microbes_16S_gamma_partition_additive_richness_prop.png", width=4, height=3 )
+
+
+## FAMILY LEVEL adipart to compare ###
+### Read table metadata and abundances
+microbes_16S_family <- read.csv("/Users/bia/PostDoc/projects/Calvert_O-Connor_eelgrass/Data/prokaryotes/family_16S_core_microbes.csv", header=T)
+names(microbes_16S_family)[1:17]
+
+# split by year
+comm.years_family <- split( microbes_16S_family, microbes_16S_family$year )
+lapply(comm.years_family, dim)
+
+# remove taxa that did not appear in a given year
+comm.zero_family <- lapply( comm.years_family, function(z) {
+  tmp <- z[,-c(1:7)]
+  w <- colSums(tmp)>0
+  df <- tmp[, w ]
+  return( data.frame(z[,c(1:7)],df))
+})
+
+meta_family <- lapply(comm.zero_family, function(z) z[,1:7])
+d_family <- lapply(comm.zero_family, function(z) z[,-c(1:7)])
+
+x_family <- lapply( meta_family, function(z) data.frame(z[,c("site_quadrat_id", "site", "region")],whole=1) )
+
+adds_family <- mapply( function(comm,x_family) adipart(comm,x_family, index="richness",weights="prop", 
+                                         relative=T, nsimul = 20 ), 
+                d_family, x_family )
+adds.mean_family <- lapply( adds_family[c(2,4,6,8)], function(z) z$means[-c(2,3,4)] )
+adds.obs_family <- lapply(adds_family[c(1,3,5,7)], function(z) z[-c(2,3,4)] )
+lapply( adds_family[c(2,4,6,8)], function(z) data.frame(z[c("z","pval")]) )
+
+add.plot_family <- data.frame( year=rep(c(2015:2018),each=4),
+                        level=rep(c("alpha","beta_sample","beta_site","beta_region"),4), 
+                        observed=do.call( c, adds.obs_family ), null=do.call( c, adds.mean_family ) )
+add.plot_family$level <- factor( add.plot_family$level, levels=c("alpha","beta_sample","beta_site","beta_region") )
+ggplot( data=add.plot_family, aes(x=year,fill=level,y=observed)) + geom_bar(stat='identity')
+
+add.plot2_family <- add.plot_family %>% 
+  gather( value="value", key = "key", - year, -level)
+write.csv(add.plot2_family, file="R_Code_and_Analysis/output_data/adipart_16S_family_level.csv", row.names = F)
+
+## GENUS LEVEL adipart to compare ###
+### Read table metadata and abundances
+microbes_16S_genus <- read.csv("/Users/bia/PostDoc/projects/Calvert_O-Connor_eelgrass/Data/prokaryotes/genus_16S_core_microbes.csv", header=T)
+names(microbes_16S_genus)[1:17]
+
+# split by year
+comm.years_genus <- split( microbes_16S_genus, microbes_16S_genus$year )
+lapply(comm.years_genus, dim)
+
+# remove taxa that did not appear in a given year
+comm.zero_genus <- lapply( comm.years_genus, function(z) {
+  tmp <- z[,-c(1:7)]
+  w <- colSums(tmp)>0
+  df <- tmp[, w ]
+  return( data.frame(z[,c(1:7)],df))
+})
+
+meta_genus <- lapply(comm.zero_genus, function(z) z[,1:7])
+d_genus <- lapply(comm.zero_genus, function(z) z[,-c(1:7)])
+
+x_genus <- lapply( meta_genus, function(z) data.frame(z[,c("site_quadrat_id", "site", "region")],whole=1) )
+
+adds_genus <- mapply( function(comm,x_genus) adipart(comm,x_genus, index="richness",weights="prop", 
+                                                       relative=T, nsimul = 20 ), 
+                       d_genus, x_genus )
+adds.mean_genus <- lapply( adds_genus[c(2,4,6,8)], function(z) z$means[-c(2,3,4)] )
+adds.obs_genus <- lapply(adds_genus[c(1,3,5,7)], function(z) z[-c(2,3,4)] )
+lapply( adds_genus[c(2,4,6,8)], function(z) data.frame(z[c("z","pval")]) )
+
+add.plot_genus <- data.frame( year=rep(c(2015:2018),each=4),
+                               level=rep(c("alpha","beta_sample","beta_site","beta_region"),4), 
+                               observed=do.call( c, adds.obs_genus ), null=do.call( c, adds.mean_genus ) )
+add.plot_genus$level <- factor( add.plot_genus$level, levels=c("alpha","beta_sample","beta_site","beta_region") )
+ggplot( data=add.plot_genus, aes(x=year,fill=level,y=observed)) + geom_bar(stat='identity')
+
+add.plot2_genus <- add.plot_genus %>% 
+  gather( value="value", key = "key", - year, -level)
+write.csv(add.plot2_genus, file="R_Code_and_Analysis/output_data/adipart_16S_genus_level.csv", row.names = F)
+
 
 #############################
 ############ 18S ############
