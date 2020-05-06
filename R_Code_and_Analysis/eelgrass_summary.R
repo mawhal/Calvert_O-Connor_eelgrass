d84468e5b2d2b00a806d4cd7cdf8afeb7c446ddc
Matt Whalen <mawhal@gmail.com>
2020-05-06 12:25:46 -0700

    add PCA for eelgrass characteristics (biomass, density, LAI, etc.)

--- R_Code_and_Analysis/eelgrass_summary.R
+++ R_Code_and_Analysis/eelgrass_summary.R
@@ -47,10 +47,13 @@ qsa <- left_join( qs, abiotic )
 
 
 # 
+psych::pairs.panels( select(qsa,shoot.density,biomass,shoot.length,shoot.width,lai), scale = F )
+psych::pairs.panels( qsa %>% select(shoot.density,biomass,shoot.length,shoot.width,lai) %>% 
+                       mutate(shoot.density=log(shoot.density), biomass=log(biomass), lai=log(lai)), scale = F )
 pp <- select(qsa, depth_m, area_m2, shoot.density, biomass, shoot.length, shoot.width)
 ppl <- pp %>% mutate( area_m2=log(area_m2), shoot.density=log(shoot.density), biomass=log(biomass) )
-# pp <- select(qsa, shoot.density, biomass, shoot.length, shoot.width)
-# ppl <- pp %>% mutate(  shoot.density=log(shoot.density), biomass=log(biomass) )
+pp <- select(qsa, shoot.density, biomass, shoot.length, shoot.width)
+ppl <- pp %>% mutate(  shoot.density=log(shoot.density), biomass=log(biomass) )
 psych::pairs.panels( ppl, scale = T )
 
 
@@ -67,19 +70,19 @@ autoplot( prcomp(ppl), data=ppp, col='site',
           loadings.label = T, loadings.label.colour="black", 
           loadings.label.size = 2,  
           scale = 1  )
-ggsave( "figs/eelgrass_PCA12.png", width=5, height=3 )
+ggsave( "figs/eelgrass_PCA_biotic12.png", width=5, height=3 )
 autoplot( prcomp(ppl), data=ppp, col='site', x=2, y=3,
           loadings = T, loadings.colour="blue",
           loadings.label = T, loadings.label.colour="black", 
           loadings.label.size = 2,  
           scale = 1  )
-ggsave( "figs/eelgrass_PCA23.png", width=5, height=3 )
+ggsave( "figs/eelgrass_PCA_biotic23.png", width=5, height=3 )
 autoplot( prcomp(ppl), data=ppp, col='site', x=1, y=4,
           loadings = T, loadings.colour="blue",
           loadings.label = T, loadings.label.colour="black", 
           loadings.label.size = 2,  
           scale = 1  )
-ggsave( "figs/eelgrass_PCA14.png", width=5, height=3 )
+ggsave( "figs/eelgrass_PCA_14.png", width=5, height=3 )
 autoplot( prcomp(ppl), data=ppp, col='site', x=4, y=5,
           loadings = T, loadings.colour="blue",
           loadings.label = T, loadings.label.colour="black", 
@@ -93,6 +96,9 @@ ppm <- qsa %>%
   ungroup()
 pp <- select(ppm, depth_m, area_m2, shoot.density, biomass, shoot.length, shoot.width)
 ppl <- pp %>% mutate( area_m2=log(area_m2), shoot.density=log(shoot.density), biomass=log(biomass) )
+pp <- select(ppm, shoot.density, biomass, shoot.length, shoot.width)
+ppl <- pp %>% mutate( shoot.density=log(shoot.density), biomass=log(biomass) )
+
 psych::pairs.panels( ppl, scale = T )
 
 
@@ -104,4 +110,41 @@ autoplot( prcomp(ppl), data=ppm, col='site',
           loadings.label = T, loadings.label.colour="black", 
           loadings.label.size = 2,  
           scale = 1  )
-ggsave( "figs/eelgrass_PCAmean12.png", width=5, height=3 )
+ggsave( "figs/eelgrass_PCA_bioticmean12.png", width=5, height=3 )
+autoplot( prcomp(ppl), data=ppm, x=2, y=3,
+          col='site', 
+          loadings = T, loadings.colour="blue",
+          loadings.label = T, loadings.label.colour="black", 
+          loadings.label.size = 2,  
+          scale = 1  )
+ggsave( "figs/eelgrass_PCA_bioticmean23.png", width=5, height=3 )
+
+qsa$site <- fct_reorder(qsa$site,qsa$shoot.length)
+ggplot( qsa, aes(x=site,y=shoot.length)) + geom_boxplot() +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))
+ppm$site <- fct_reorder(ppm$site,ppm$shoot.length)
+ggplot( ppm, aes(x=site,y=shoot.length)) + geom_boxplot() +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +
+  theme(axis.text=element_text(size=6))
+vegan::make.cepnames(unique(ppm$site))
+ggsave( "figs/eelgrass_shoot_length.png", width=4,height=3 )
+
+
+
+# replace LAI with other variables
+pp <- select(qsa, depth_m,  biomass, lai ) #area_m2,
+ppl <- pp %>% mutate( biomass=log(biomass), lai=log(lai) ) #area_m2=log(area_m2), 
+# PCA
+pca <- prcomp( ppl )
+summary(pca)
+autoplot( prcomp(ppl), data=qsa, col='site', 
+          loadings = T, loadings.colour="blue",
+          loadings.label = T, loadings.label.colour="black", 
+          loadings.label.size = 2,  
+          scale = 1  )
+qsa$site <- fct_reorder(qsa$site,qsa$biomass)
+ggplot( qsa, aes(x=site,y=biomass)) + geom_boxplot() +
+  scale_x_discrete(guide = guide_axis(n.dodge = 2))+
+  scale_y_continuous(trans="log10") +
+  theme(axis.text=element_text(size=6))
+ggsave( "figs/eelgrass_biomass.png", width=4,height=3 )
