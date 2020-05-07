c1f227dd6552b4bf33a6a528fb96f6736a3dda43
Matt Whalen <mawhal@gmail.com>
2020-05-07 11:06:14 -0700

    procrustes: calculate all pairs within a specific year

--- R_Code_and_Analysis/procrustes/procrustes_test.R
+++ R_Code_and_Analysis/procrustes/procrustes_test.R
@@ -6,58 +6,129 @@ library(tidyverse)
 library(vegan)
 #library(usedist)
 
-###################################
-############### 2016 ##############
-###################################
+# pick a year
+year <- 2016
 
 #load grazers metadata
-metadata_grazers_2016 <- read_csv("R_Code_and_Analysis/output_data/2016_grazer_metadata.csv")
+metadata_macro <- read_csv(paste0("R_Code_and_Analysis/mantel/",year,"_macroeuk_metadata.csv") )
 
 #The row and column names in the distance matrix can be created from metadata$sample using vegan::make.cepnames()
-metadata_grazers_2016$labels <- vegan::make.cepnames(metadata_grazers_2016$sample)
+metadata_macro$labels <- vegan::make.cepnames(metadata_macro$sample)
 
 #load grazers bray curtis dissimilarity matrix
-df_grazers_2016 <- read.csv("R_Code_and_Analysis/mantel_microbes_grazers/2016_grazer_braycurtis.csv")
+df_macro <- read_csv(paste0("R_Code_and_Analysis/mantel/",year,"_macroeuk_braycurtis.csv") )
 
-df_grazers_2016$sample <- colnames(df_grazers_2016)
+df_macro$sample <- colnames(df_macro)
 
 #load 16S microbial distance matrix GENUS
-df_16S_2016_genus <- read.csv("R_Code_and_Analysis/mantel_microbes_grazers/genus_16S_2016_braycurtis.csv")
-df_16S_2016_genus <- df_16S_2016_genus %>% 
-  dplyr::rename("sample" = "X")
+df_16S_genus <- read_csv(paste0("R_Code_and_Analysis/mantel/genus_16S_",year,"_braycurtis.csv") )
+df_16S_genus <- df_16S_genus %>% 
+  dplyr::rename("sample" = "X1")
 
 #load 18S microbial distance matrix GENUS
-df_18S_2016_genus <- read.csv("R_Code_and_Analysis/mantel_microbes_grazers/genus_18S_2016_braycurtis.csv")
-df_18S_2016_genus <- df_18S_2016_genus %>% 
-  dplyr::rename("sample" = "X")
+df_18S_genus <- read_csv(paste0("R_Code_and_Analysis/mantel/genus_18S_",year,"_braycurtis.csv") )
+df_18S_genus <- df_18S_genus %>% 
+  dplyr::rename("sample" = "X1")
 
 
 ##############################################
-######### PROCRUSTES GRAZERS VS 16S GENUS ######### 
+######### PROCRUSTES MACROEukaryotes VS 16S GENUS ######### 
 ##############################################
 
 # which samples are common to both matrices?
-labels_grazers_2016 <- as.vector(df_grazers_2016$sample)
-labels_16S_genus_2016 <- as.vector(df_16S_2016_genus$sample)
+labels_macro <- as.vector(df_macro$sample)
+# labels_macro <- as.vector(metadata_macro$labels)
+labels_16S_genus <- as.vector(df_16S_genus$sample)
+labels_18S_genus <- as.vector(df_18S_genus$sample)
 
 # filter only samples that are common to both 
-common_samples <- intersect(labels_grazers_2016, labels_16S_genus_2016)
+common_samples <- intersect(labels_macro, labels_16S_genus)
 
-df_grazers_2016_mantel <- df_grazers_2016 %>% 
+df_macro_mantel <- df_macro %>% 
   dplyr::filter(sample %in% common_samples)
-dist_grazers_2016 <- dist(df_grazers_2016_mantel, diag = TRUE, upper = FALSE)
+dist_macro <- dist(df_macro_mantel, diag = TRUE, upper = FALSE)
 
-df_16S_genus_2016_mantel <- df_16S_2016_genus %>% 
+df_16S_genus_mantel <- df_16S_genus %>% 
   dplyr::filter(sample %in% common_samples)
-dist_16S_genus_2016 <- dist(df_16S_genus_2016_mantel , diag = TRUE, upper = FALSE)
+dist_16S_genus <- dist(df_16S_genus_mantel , diag = TRUE, upper = FALSE)
 
 
-mds.invert <- monoMDS(dist_grazers_2016)
-mds.16S <- monoMDS(dist_16S_genus_2016)
-proc_invert_16S <- procrustes(mds.invert, mds.16S)
-proc_invert_16S 
-summary(proc_invert_16S)
-plot(proc_invert_16S)
-plot(proc_invert_16S, kind=2)
-residuals(proc_invert_16S)
-protest(mds.invert, mds.16S)
+mds.target <- monoMDS(dist_macro)
+mds.rotated <- monoMDS(dist_16S_genus)
+proc_macro_16S <- procrustes(mds.target, mds.rotated)
+proc_macro_16S 
+summary(proc_macro_16S)
+plot(proc_macro_16S)
+plot(proc_macro_16S, kind=2)
+residuals(proc_macro_16S)
+p1 <- protest(mds.target, mds.rotated)
+p1$signif
+p1$ss
+
+##############################################
+######### PROCRUSTES MACROEukaryotes VS 18S GENUS ######### 
+##############################################
+
+# which samples are common to both matrices?
+
+# filter only samples that are common to both 
+common_samples <- intersect(labels_macro, labels_18S_genus)
+
+df_macro_mantel <- df_macro %>% 
+  dplyr::filter(sample %in% common_samples)
+dist_macro <- dist(df_macro_mantel, diag = TRUE, upper = FALSE)
+
+df_18S_genus_mantel <- df_18S_genus %>% 
+  dplyr::filter(sample %in% common_samples)
+dist_18S_genus <- dist(df_18S_genus_mantel , diag = TRUE, upper = FALSE)
+
+
+mds.target <- monoMDS(dist_macro)
+mds.rotated <- monoMDS(dist_18S_genus)
+proc_macro_16S <- procrustes(mds.target, mds.rotated)
+proc_macro_16S 
+summary(proc_macro_16S)
+plot(proc_macro_16S)
+plot(proc_macro_16S, kind=2)
+residuals(proc_macro_16S)
+p2 <- protest(mds.target, mds.rotated)
+p2$signif
+p2$ss
+
+##############################################
+######### PROCRUSTES 18S GENUS VS 16S GENUS ######### 
+##############################################
+
+# which samples are common to both matrices?
+
+# filter only samples that are common to both 
+common_samples <- intersect(labels_18S_genus, labels_16S_genus)
+
+df_16S_genus_mantel <- df_16S_genus %>% 
+  dplyr::filter(sample %in% common_samples)
+dist_16S_genus <- dist(df_16S_genus_mantel , diag = TRUE, upper = FALSE)
+df_18S_genus_mantel <- df_18S_genus %>% 
+  dplyr::filter(sample %in% common_samples)
+dist_18S_genus <- dist(df_18S_genus_mantel , diag = TRUE, upper = FALSE)
+
+
+mds.target <- monoMDS(dist_18S_genus)
+mds.rotated <- monoMDS(dist_16S_genus)
+proc_macro_16S <- procrustes(mds.target, mds.rotated)
+proc_macro_16S 
+summary(proc_macro_16S)
+plot(proc_macro_16S)
+plot(proc_macro_16S, kind=2)
+residuals(proc_macro_16S)
+p3 <- protest(mds.target, mds.rotated, permutations = how(nperm = 9999) )
+p3$signif
+p3$ss
+# sqrt( 1-p3$ss )
+
+
+# wrap results together
+res <- data.frame(pair=c("macro-16S","macro-18S","18S-16S"),matrix( c(p1$ss,p1$signif,p2$ss,p2$signif,p3$ss,p3$signif), ncol=2, byrow=T ))
+names(res) <- c('pair','ss',"signif")
+
+res$corr <- sqrt(1-res$ss)
+res$year <- year
