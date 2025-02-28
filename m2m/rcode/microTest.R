4030302139a268909b1a32076c2ac327a6429e72
DeirdreLoughnan <deirdre.loughnan@gmail.com>
2025-02-27 17:07:47 -0800

    updated micro pp on yr

--- m2m/rcode/microTest.R
+++ m2m/rcode/microTest.R
@@ -1,3 +1,5 @@
+# code started Feb 27, 2025 by D. Loughnan
+# aim of this code is to develop test data to jointly model changes in microbial diversity and how it impacts changes in macro diversity.
 rm(list=ls())
 options(stringsAsFactors = FALSE)
 options(mc.cores = parallel::detectCores())
@@ -6,7 +8,6 @@ library(ggplot2)
 library(rstan)
 library(shinystan)
 
-#setwd("~/Documents/git/projects/treegarden/budreview/ospree/analyses/ranges")
 if(length(grep("deirdre", getwd())>0)) {  setwd("~/Documents/github/Calvert_O-Connor_eelgrass")
 } else if
 (length(grep("temp", getwd())>0)) {   setwd("~/Documents/git/temp")
@@ -17,40 +18,50 @@ options(stringsAsFactors = FALSE)
 options(mc.cores = parallel::detectCores())
 
 Nquad <- 20# quadrat
-Nsite <- 25 # sites (choked, Triquet etc)
-Nyear <- 25# number of year with microbial
+Nsite <- 30 # sites (choked, Triquet etc)
+Nyear <- 30# number of year with microbial
 
 # First making a data frame for the test micro data
-Ntrt <- Nyear * Nsite * Nquad# total number of micro observations
-Ntrt
+Nmicro <- Nyear * Nsite * Nquad# total number of micro observations
+Nmicro
 
 #make a data frame 
-trt.dat <- data.frame(matrix(NA, Ntrt, 1))
-names(trt.dat) <- c("iter"); trt.dat$iter <- 1:nrow(trt.dat)
-trt.dat$rep <- c(1:Nquad)
-trt.dat$year <- rep(1:Nyear, each = Nquad)
-trt.dat$site <- rep(1:Nsite, each = Nyear*Nquad)
+micro.dat <- data.frame(matrix(NA, Nmicro, 1))
+names(micro.dat) <- c("iter"); micro.dat$iter <- 1:nrow(micro.dat)
+micro.dat$rep <- c(1:Nquad)
+micro.dat$site <- rep(1:Nsite, each = Nquad)
+micro.dat$year <- rep(1:Nyear, each = Nquad*Nsite)
 
 mu.grand <- 10 # the grand mean of the micro model
 sigma.site <- 1 # we want to keep the variation across sites high
 
 mu.site <- rnorm(Nsite, 0, sigma.site)
-trt.dat$mu.site <- rep(mu.site + mu.grand, each = Nquad*Nyear) 
+micro.dat$mu.site <- rep(mu.site + mu.grand, each = Nquad) 
 
 #now generating the effects of year
+yeari <- rnorm(Nsite*Nyear, 10, 1)
+micro.dat$yeari <- (rep(yeari, each = Nquad))
+
 mu.year <- 4
+sigma.year = 1
+alpha.year.site <- rnorm(Nsite, mu.year, sigma.year)
+micro.dat$alphayearSite <- rep(alpha.year.site, each = Nquad)
 
 # general variance
-trt.var <- 1 
-trt.dat$trt.er <- rnorm(Ntrt, 0, trt.var)
+micro.var <- 1 
+micro.dat$micro.er <- rnorm(Nmicro, 0, micro.var)
 
-trt.dat$yMicroi <- trt.dat$mu.site +  trt.dat$trt.er + mu.year * trt.dat$year
+#micro.dat$yMicroi <- micro.dat$mu.site +  micro.dat$micro.er + micro.dat$alphayearSite * micro.dat$year
 
-micro_data <- list(yMicroi = trt.dat$yMicroi,
-                   N = Ntrt,
-                   n_site = Nsite,
-                   micro_site = trt.dat$site,
-                   yeari = trt.dat$year
+for (i in 1:Nmicro){
+  micro.dat$yMicroi2[i] <-  micro.dat$mu.site[i] +  micro.dat$micro.er[i] + micro.dat$alphayearSite[i]  * micro.dat$yeari[i] 
+    }
+
+micro_data <- list(yMicroi = micro.dat$yMicroi,
+                   N = Nmicro,
+                   n_sites = Nsite,
+                   micro_sites = micro.dat$site,
+                   yeari = micro.dat$yeari
                    )
 
 mdl.micro <- stan('m2m/stan/microMdl.stan',
@@ -61,8 +72,14 @@ mdl.micro <- stan('m2m/stan/microMdl.stan',
                   include = FALSE, pars = c("ypred_new","y_hat")
 )
 
-sumer <- summary(mdl.micro)$summary[c("mu_grand","b_year","sigma_site","sigma_Microy"),]; sumer
+sumer <- summary(mdl.micro)$summary[c("mu_grand","muYear", "sigmaYear","sigma_site","sigma_Microy"),]; sumer
 
+#                  mean      se_mean          sd      2.5%       25%        50%        75%      97.5%     n_eff      Rhat
+# mu_grand  10   10.0862238 0.0065285949 0.206007884 9.6774653 9.9533429 10.0839305 10.2190603 10.4924307  995.6994 1.0024579
+# muYear    4    4.0026940 0.0020930133 0.161715515 3.6746745 3.8971607  4.0040823  4.1104909  4.3154459 5969.7948 0.9992787
+# sigmaYear  1   0.9078530 0.0015954714 0.126805947 0.7021279 0.8166935  0.8944491  0.9822854  1.1887270 6316.8591 0.9997024
+# sigma_site  1  0.9999804 0.0031526018 0.166878513 0.7147537 0.8855282  0.9828262  1.1027991  1.3586601 2801.9643 1.0004308
+# sigma_Microy 1 0.9891385 0.0000541771 0.005236842 0.9790957 0.9855876  0.9890563  0.9926463  0.9997088 9343.4533 0.9993346
 
 ssm <-  as.shinystan(mdl.micro)
 launch_shinystan(ssm)
--- m2m/stan/microMdl.stan
+++ m2m/stan/microMdl.stan
@@ -1,8 +1,8 @@
 data {
-  int<lower = 1> n_site; // number of random effect levels (sites) 
+  int<lower = 1> n_sites; // number of random effect levels (sites) 
 
   int<lower = 1> N; // Sample size for micro data 
-  array[N] int<lower = 0> micro_site; // id of random effect (sites)
+  array[N] int<lower = 0> micro_sites; // id of random effect (sites)
   
   vector[N] yeari;
   vector[N] yMicroi; // Observed micro diversity
@@ -11,10 +11,12 @@ data {
 parameters{
   // Traits
   real mu_grand; // grand mean 
-  array[n_site] real b_muSite;
+  array[n_sites] real b_muSite;
   
-  real b_year;
-  
+  array[n_sites] real alphaYear;
+  real muYear;
+  real<lower = 0> sigmaYear;
+
   real<lower = 0> sigma_Microy; // sd general
   real<lower = 0> sigma_site; // sd site
   
@@ -24,17 +26,22 @@ transformed parameters{
   vector[N] y_hat;
 
   for (i in 1:N){
-    y_hat[i] =   mu_grand + b_year * yeari[i]  + b_muSite[micro_site[i]]
+    y_hat[i] =   mu_grand  + b_muSite[micro_sites[i]] + alphaYear[micro_sites[i]] * yeari[i]
     ;
   }
 
 }
 
 model{
+  alphaYear ~ normal(muYear, sigmaYear);
+
   b_muSite ~ normal(0, sigma_site);
   mu_grand ~ normal(10,10);
-  b_year ~ normal(0,5);
+  
+  muYear ~ normal(0,15);
+  sigmaYear ~ normal(0,15);
   sigma_site ~ normal(4,5);
+
   sigma_Microy ~ normal(3, 5);
   
   yMicroi ~ normal(y_hat, sigma_Microy);
